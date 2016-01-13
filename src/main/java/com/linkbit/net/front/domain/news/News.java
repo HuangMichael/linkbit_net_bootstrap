2d92e2c349cebba8bebd4bdaae9249b4fe91d3d3
876301469@qq.com <876301469@qq.com>
2016-01-13 14:03:48 +0800

    add news

---
+++ src/main/java/com/linkbit/net/front/domain/news/News.java
@@ -0,0 +1,28 @@
+package com.linkbit.net.front.domain.news;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by huangbin on 2016/1/12 0012.
+ * 知识库实体类
+ */
+
+@Entity
+@Table(name = "T_NEWS")
+@Data
+public class News {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String newsTitle;
+    private String newsDesc;
+    private String imgUrl;
+    private String newsContent;
+    private Date publishTime;
+    private String keywords;
+    private Boolean status;
+}
---
+++ src/main/java/com/linkbit/net/front/domain/news/NewsRepository.java
@@ -0,0 +1,22 @@
+package com.linkbit.net.front.domain.news;
+
+
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+/**
+ * Created by huangbin on 2016/1/13 0013.
+ */
+public interface NewsRepository extends Repository<News, Long> {
+
+    /**
+     * 查询所有的新闻信息
+     */
+    List<News> findAll();
+
+    /**
+     * 根据id查询对应的新闻信息
+     */
+    News findById(Long id);
+}
