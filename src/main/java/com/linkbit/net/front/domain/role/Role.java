24265b0521b8b7b7929b0992eb74f68af7141811
huangbin <876301469@qq.com>
2016-03-08 17:58:04 +0800

    添加角色加用户的功能 保存失败

--- src/main/java/com/linkbit/net/front/domain/role/Role.java
+++ src/main/java/com/linkbit/net/front/domain/role/Role.java
@@ -1,10 +1,12 @@
 package com.linkbit.net.front.domain.role;
 
+import com.linkbit.net.front.domain.user.User;
 import lombok.*;
 import lombok.extern.log4j.Log4j;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Created by huangbin on 2016/3/8.
@@ -27,4 +29,6 @@ public class Role implements Serializable {
     private String roleDesc;
     @Column(length = 1,nullable = false)
     private String status;
+    @ManyToMany(mappedBy="roles",cascade=CascadeType.ALL , targetEntity = com.linkbit.net.front.domain.user.User.class)
+    private Set<User> users;
 }
