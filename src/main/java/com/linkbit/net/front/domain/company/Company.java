45c7f7a3cc5556205af419bef2bb94b010e5fb7a
huangbin <876301469@qq.com>
2016-03-10 09:32:48 +0800

    加入字段长度和是否可为空限制

--- src/main/java/com/linkbit/net/front/domain/company/Company.java
+++ src/main/java/com/linkbit/net/front/domain/company/Company.java
@@ -26,14 +26,22 @@ public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;//id
+    @Column(length = 50,nullable = false)
     private String companyName;//公司名称
+    @Column(length = 50)
     private String logoPath;//logo地址
+    @Column(length = 100,nullable = false)
     private String address;//公司地址
+    @Column(length = 10)
     private String longtitude;  //维度
+    @Column(length = 10)
     private String latitude;  //经度
+    @Column(length = 12)
     private String telephone; //  电话
+    @Column(length = 12)
     private String fax;//传真
     private Date buildDate;//成立日期
+    @Column(length = 1,nullable = false)
     private Boolean status;//状态
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<CompanyProperty> companyPropertyList  = new ArrayList<CompanyProperty>();
