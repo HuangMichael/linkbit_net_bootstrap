7352dd3caca00e63801441b742613d81f78b0d9a
huangbin <876301469@qq.com>
2016-03-11 14:21:44 +0800

    留言信息实体类加入字段属性描述

--- src/main/java/com/linkbit/net/front/domain/message/Message.java
+++ src/main/java/com/linkbit/net/front/domain/message/Message.java
@@ -1,6 +1,5 @@
 package com.linkbit.net.front.domain.message;
 
-import jdk.nashorn.internal.objects.annotations.Constructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,12 +21,18 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(length = 20, nullable = true)
     private String userName;
+    @Column(length = 20, nullable = true)
     private String email;
+    @Column(length = 20, nullable = true)
     private String telephone;
+    @Column(length = 50, nullable = true)
     private String workplace;
+    @Column(length = 200, nullable = true)
     private String content;
     @Temporal(TemporalType.TIMESTAMP)
     private Date messageTime;
+    @Column(length = 1, nullable = false)
     private String status;
 }
