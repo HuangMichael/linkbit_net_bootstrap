f34d14e9732bda39e9bed192a6602ee8382de6a7
huangbin <876301469@qq.com>
2016-03-14 11:47:32 +0800

    作者注释

--- src/main/java/com/linkbit/net/front/domain/knowledge/KnowledgeRepository.java
+++ src/main/java/com/linkbit/net/front/domain/knowledge/KnowledgeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.Repository;
 import java.util.List;
 
 /**
- * Created by Administrator on 2016/1/12 0012.
+ * Created by huangbin on 2016/1/12 0012.
  */
 public interface KnowledgeRepository extends Repository<Knowledge, Long> {
 
